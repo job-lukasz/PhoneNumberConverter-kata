53f56a366fc7d6b1a9232fd5f71050642781ecff
job-lukasz <job.lukasz.1990@gmail.com>
2014-05-13 01:02:40 +0200

    Code refactor -new Digit Class

--- src/main/java/converter/NumberConverter.java
+++ src/main/java/converter/NumberConverter.java
@@ -6,8 +6,6 @@ import java.util.TreeMap;
 
 public class NumberConverter {
 
-	private char numberArr[];
-
 	private static final Map<Character, char[]> numberMap;
 	static {
 		Map<Character, char[]> tempMap = new TreeMap<Character, char[]>();
@@ -17,47 +15,75 @@ public class NumberConverter {
 		numberMap = Collections.unmodifiableMap(tempMap);
 	}
 
-	public String convert(String number) {
-		String result = "";
-		boolean cursorRight = true;
-		int cursorPosition = 0;
-		numberArr = new char[9];
-		int numberCursorIteration[] = new int[9];
-		int maxCursorIteration[] = new int[9];
-		for (int i = 0; i < 9; i++) {
-			numberArr[i] = number.charAt(i);
-			numberCursorIteration[i] = -1;
-			if (numberArr[i] > '1') {
-				maxCursorIteration[i] = 3;
-			} else {
-				maxCursorIteration[i] = 1;
-			}
+	private class Digit {
+		public int currentCursorIteration;
+		public char converterValue[];
+
+		public Digit(char value) {
+			currentCursorIteration = -1;
+			converterValue = numberMap.get(value);
 		}
-		String tempWord = "";
-		while ((cursorRight == true) || (cursorPosition != -1)) {
-			numberCursorIteration[cursorPosition]++;
-			if (!cursorRight && tempWord.length() > 0) {
-				tempWord = tempWord.substring(0, tempWord.length() - 1);
-			}
-			if (numberCursorIteration[cursorPosition] < maxCursorIteration[cursorPosition]) {
-				tempWord += numberMap.get(numberArr[cursorPosition])[numberCursorIteration[cursorPosition]];
 
-				cursorRight = true;
-				cursorPosition++;
+		public char getCurrentDigit() {
+			return converterValue[currentCursorIteration];
+		}
 
-				if (cursorPosition < 9) {
-					numberCursorIteration[cursorPosition] = -1;
-				} else {
-					result += tempWord + "\n";
+		public boolean isMoreValue() {
+			return currentCursorIteration < converterValue.length;
+		}
+	}
 
-					cursorRight = false;
-					cursorPosition--;
-				}
+	private Digit digits[];
+	private String result = "";
+	private boolean cursorRight = true;
+	private int cursorPosition = 0;
+	private String currWord = "";
+	
+	public String convert(String number) {
+		initDigits(number);
+		while (isAnythingToDo()) {
+			digits[cursorPosition].currentCursorIteration++;
+			if (digits[cursorPosition].isMoreValue()) {
+				currWord += digits[cursorPosition].getCurrentDigit();
+				moveCursorRight();
 			} else {
-				cursorRight = false;
-				cursorPosition--;
+				moveCursorLeft();
 			}
 		}
 		return result.substring(0, result.length() - 1);
+	}
+
+	private void deleteCursoredChar() {
+		if (currWord.length() > 0) {
+			currWord = currWord.substring(0, currWord.length() - 1);
+		}
+	}
+
+	private void moveCursorLeft() {
+		cursorRight = false;
+		cursorPosition--;
+		deleteCursoredChar();
+	}
+
+	private void moveCursorRight() {
+		cursorRight = true;
+		cursorPosition++;
+		if (cursorPosition < 9) {
+			digits[cursorPosition].currentCursorIteration = -1;
+		} else {
+			result += currWord + "\n";
+			moveCursorLeft();
+		}
+	}
+
+	private boolean isAnythingToDo() {
+		return (cursorRight == true) || (cursorPosition != -1);
+	}
+
+	private void initDigits(String number) {
+		digits = new Digit[9];
+		for (int i = 0; i < 9; i++) {
+			digits[i] = new Digit(number.charAt(i));
+		}
 	}
 }

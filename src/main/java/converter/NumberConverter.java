aee9a6c6fb28849b4b1595deb3d6de7e4f4d19e0
job-lukasz <job.lukasz.1990@gmail.com>
2014-05-15 11:10:05 +0200

    Add functionality of converting all posibility numbers

--- src/main/java/converter/NumberConverter.java
+++ src/main/java/converter/NumberConverter.java
@@ -11,25 +11,38 @@ public class NumberConverter {
 		Map<Character, char[]> tempMap = new TreeMap<Character, char[]>();
 		tempMap.put('0', new char[] { '0' });
 		tempMap.put('1', new char[] { '1' });
-		tempMap.put('2', new char[] { 'A', 'B', 'C' });
+		tempMap.put('2', new char[] { 'A', 'B', 'C'});
+		tempMap.put('3', new char[] { 'D', 'E', 'F'});
+		tempMap.put('4', new char[] { 'G', 'H', 'I'});
+		tempMap.put('5', new char[] { 'J', 'K', 'L'});
+		tempMap.put('6', new char[] { 'M', 'N', 'O'});
+		tempMap.put('7', new char[] { 'P', 'Q', 'R', 'S'});
+		tempMap.put('8', new char[] { 'T', 'U', 'V'});
+		tempMap.put('9', new char[] { 'W', 'X', 'Y', 'Z'});
 		numberMap = Collections.unmodifiableMap(tempMap);
 	}
 
 	private class Digit {
-		public int currentCursorIteration;
-		public char converterValue[];
+		private int digitIteration;
+		private char converterValue[];
 
 		public Digit(char value) {
-			currentCursorIteration = -1;
+			digitIteration = -1;
 			converterValue = numberMap.get(value);
 		}
 
 		public char getCurrentDigit() {
-			return converterValue[currentCursorIteration];
+			return converterValue[digitIteration];
 		}
 
 		public boolean isMoreValue() {
-			return currentCursorIteration < converterValue.length;
+			return digitIteration < converterValue.length;
+		}
+		public void increaseIteration(){
+			digitIteration++;
+		}
+		public void resetIteration(){
+			digitIteration = -1;
 		}
 	}
 
@@ -42,7 +55,7 @@ public class NumberConverter {
 	public String convert(String number) {
 		initDigits(number);
 		while (isAnythingToDo()) {
-			digits[cursorPosition].currentCursorIteration++;
+			digits[cursorPosition].increaseIteration();
 			if (digits[cursorPosition].isMoreValue()) {
 				currWord += digits[cursorPosition].getCurrentDigit();
 				moveCursorRight();
@@ -50,30 +63,39 @@ public class NumberConverter {
 				moveCursorLeft();
 			}
 		}
+		return resultWithoutLastNL();
+	}
+
+	private String resultWithoutLastNL() {
 		return result.substring(0, result.length() - 1);
 	}
 
-	private void deleteCursoredChar() {
+	private void deleteLastChar() {
+		if (cursorPosition < 9) {
+			digits[cursorPosition].resetIteration();
+		}
 		if (currWord.length() > 0) {
 			currWord = currWord.substring(0, currWord.length() - 1);
 		}
 	}
 
 	private void moveCursorLeft() {
+		deleteLastChar();
 		cursorRight = false;
 		cursorPosition--;
-		deleteCursoredChar();
 	}
 
 	private void moveCursorRight() {
 		cursorRight = true;
 		cursorPosition++;
-		if (cursorPosition < 9) {
-			digits[cursorPosition].currentCursorIteration = -1;
-		} else {
-			result += currWord + "\n";
+		if (cursorPosition > 8) {
+			addWordToResult();
 			moveCursorLeft();
 		}
+	}
+
+	private void addWordToResult() {
+		result += currWord + "\n";
 	}
 
 	private boolean isAnythingToDo() {
